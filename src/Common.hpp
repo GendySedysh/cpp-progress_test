4004e0979e78259ff96220a2df3a8de9e2af6837
test <test>
2024-08-15 11:53:57 +0000

    initial commit

---
+++ src/Common.hpp
@@ -0,0 +1,16 @@
+#ifndef CLIENSERVERECN_COMMON_HPP
+#define CLIENSERVERECN_COMMON_HPP
+
+#include <string>
+
+static short port = 5555;
+
+namespace Requests
+{
+    static std::string Registration = "Reg";
+    static std::string Hello = "Hel";
+    static std::string Buy = "Buy";
+    static std::string Sell = "Sell";
+}
+
+#endif //CLIENSERVERECN_COMMON_HPP
